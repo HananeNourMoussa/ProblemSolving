2f8e0103a7b8507192ef3774863c9f0588adb02e
Hanane Nour Moussa <71970059+HananeNourMoussa@users.noreply.github.com>
2021-08-30 00:13:49 +0100

    Create rotatedSearch.py

---
+++ rotatedSearch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while (left <= right): 
+            mid = (left + right)//2 
+            if (nums[mid] == target): 
+                return mid
+            if (nums[left] <= nums[mid]): 
+                if (nums[left] <= target and nums[mid] > target): 
+                    right = mid-1
+                else: 
+                    left = mid +1 
+            else: 
+                if (nums[right] >= target and nums[mid] < target): 
+                    left = mid+1 
+                else: 
+                    right = mid-1 
+        return -1
